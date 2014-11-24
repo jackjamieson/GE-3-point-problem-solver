f2ae282b571369663a8e828359a3a15b379bb04c
Jack Jamieson <jamieson.jack@gmail.com>
2014-11-24 17:20:35 -0500

    added true full screen toggle

--- js/3pp.js
+++ js/3pp.js
@@ -1149,6 +1149,28 @@ function setHeight(elem1, elem2) {
 	elem1.css('height', height); 
 }
 
+//full screen toggle
+function toggleFullScreen() {
+  if ((document.fullScreenElement && document.fullScreenElement !== null) ||    
+   (!document.mozFullScreen && !document.webkitIsFullScreen)) {
+    if (document.documentElement.requestFullScreen) {  
+      document.documentElement.requestFullScreen();  
+    } else if (document.documentElement.mozRequestFullScreen) {  
+      document.documentElement.mozRequestFullScreen();  
+    } else if (document.documentElement.webkitRequestFullScreen) {  
+      document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);  
+    }  
+  } else {  
+    if (document.cancelFullScreen) {  
+      document.cancelFullScreen();  
+    } else if (document.mozCancelFullScreen) {  
+      document.mozCancelFullScreen();  
+    } else if (document.webkitCancelFullScreen) {  
+      document.webkitCancelFullScreen();  
+    }  
+  }  
+}
+
 
 ////////////////////////////////////////////////////////////////////////////////////////
 
